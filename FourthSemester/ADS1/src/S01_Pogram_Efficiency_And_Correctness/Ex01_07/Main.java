4e110a5020eabb907abc0126d993d7bc2a442381
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-02-16 11:14:03 +0100

    Solving exercise 7

---
+++ FourthSemester/ADS1/src/S01_Pogram_Efficiency_And_Correctness/Ex01_07/Main.java
@@ -0,0 +1,23 @@
+package S01_Pogram_Efficiency_And_Correctness.Ex01_07;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("isPrime() = " + isPrime(48));
+    }
+
+    /*
+    Pseudo code:
+    for 2 ... n
+        if m divisible by i without remainder return false
+    return true
+     Time Complexity: O(n)
+     */
+    public static boolean isPrime(int number) {
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

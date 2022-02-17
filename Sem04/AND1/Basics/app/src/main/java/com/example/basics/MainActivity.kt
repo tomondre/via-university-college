a0f4416c1e1d1af04d616010bfa08e5bef718d0c
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-02-17 11:34:05 +0100

    Layout names now matches exercises from AND course

--- Sem04/AND1/Basics/app/src/main/java/com/example/basics/MainActivity.kt
+++ Sem04/AND1/Basics/app/src/main/java/com/example/basics/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.ex01_activity_main)
     }
 }

a6a76dba9e0bec9cafbd5532160bf49f57c18dbf
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-02-22 09:42:16 +0100

    Solving exercise 11

--- Sem04/AND1/S02_Interaction/app/src/main/java/com/example/s02_interaction/Ex11/MainActivity.kt
+++ Sem04/AND1/S02_Interaction/app/src/main/java/com/example/s02_interaction/Ex11/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.s02_interaction.Ex11
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.Switch
 import com.example.s02_interaction.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var switch: Switch
+    private lateinit var image: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main7)
+
+        switch = findViewById(R.id.switcho)
+        switch.setOnClickListener(::handler)
+        image = findViewById(R.id.image)
+    }
+
+    private fun handler(view: View?) {
+        if (switch.isChecked)
+        {
+            image.setImageResource(R.drawable.ic_launcher_background)
+        } else {
+            image.setImageResource(R.drawable.ic_launcher_foreground)
+        }
     }
 }
---
+++ Sem04/AND1/S02_Interaction/app/src/main/java/com/example/s02_interaction/Ex13/HorizontalActivity.kt
@@ -0,0 +1,12 @@
+package com.example.s02_interaction.Ex13
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.s02_interaction.R
+
+class HorizontalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.horizontal)
+    }
+}

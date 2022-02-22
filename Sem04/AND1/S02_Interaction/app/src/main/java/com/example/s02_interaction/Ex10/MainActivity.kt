990de5e9543bc93dfd3d8f57671e96faf13963a0
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-02-22 09:17:04 +0100

    Solving exercise 10

--- Sem04/AND1/S02_Interaction/app/src/main/java/com/example/s02_interaction/Ex10/MainActivity.kt
+++ Sem04/AND1/S02_Interaction/app/src/main/java/com/example/s02_interaction/Ex10/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.s02_interaction.Ex10
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import com.example.s02_interaction.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var outputTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main6)
+
+        outputTextView = findViewById(R.id.output_text)
+        val findViewById = findViewById<Button>(R.id.buttonito)
+        findViewById.setOnClickListener(::clickHandler)
+    }
+
+    private fun clickHandler(view: View?) {
+        outputTextView.text = resources.getString(R.string.halabala)
     }
 }
---
+++ Sem04/AND1/S02_Interaction/app/src/main/java/com/example/s02_interaction/Ex11/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.s02_interaction.Ex11
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.s02_interaction.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main7)
+    }
+}

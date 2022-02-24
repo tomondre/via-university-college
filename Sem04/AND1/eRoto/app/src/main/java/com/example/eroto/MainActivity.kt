d4812a26f0b80ad595988365b74c2f3193efa637
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-02-24 13:27:11 +0100

    Adding more menu items

--- Sem04/AND1/eRoto/app/src/main/java/com/example/eroto/MainActivity.kt
+++ Sem04/AND1/eRoto/app/src/main/java/com/example/eroto/MainActivity.kt
@@ -1,22 +1,10 @@
 package com.example.eroto
 
-import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
-import android.view.ActionMode
 import android.view.MenuItem
-import android.view.View
 import android.widget.ImageButton
-import android.widget.LinearLayout
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
-import com.github.mikephil.charting.charts.BarChart
-import com.github.mikephil.charting.charts.LineChart
-import com.github.mikephil.charting.data.*
-import com.github.mikephil.charting.utils.Utils
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
@@ -38,7 +26,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun NavigationListener(menuItem: MenuItem): Boolean {
         var fragment: Fragment = when(menuItem.itemId){
-            R.id.discover ->
+            R.id.discover_menu_item ->
                 DiscoverFragment()
             else ->
                 Main_fragment()

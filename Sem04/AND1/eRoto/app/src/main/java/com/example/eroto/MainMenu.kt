9271fedc9218ecdea81abe0c2c96eb9baf219c04
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-02-26 23:43:33 +0100

    Creating main menu

---
+++ Sem04/AND1/eRoto/app/src/main/java/com/example/eroto/MainMenu.kt
@@ -0,0 +1,11 @@
+package com.example.eroto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_menu)
+    }
+}

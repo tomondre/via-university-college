84834be51f5cb9de3e72a7bf24894d4e28274a62
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-03-01 12:44:54 +0100

    Creating exercises for session two

--- Sem04/ADS1/src/S02_lists_queues_stacks/Ex02_hasBalanceParantheases/Main.java
+++ Sem04/ADS1/src/S02_lists_queues_stacks/Ex02_hasBalanceParantheases/Main.java
@@ -1,12 +1,14 @@
-package S02_lists_queues_stacks;
+package S02_lists_queues_stacks.Ex02_hasBalanceParantheases;
+
+import S02_lists_queues_stacks.StackADT;
+import S02_lists_queues_stacks.StackImpl;
 
 public class Main {
     public static void main(String[] args) {
-
-        String expression = "(w * [x + y] / z)";
+        String expression = "(w * [x + y] /) z)";
 
         char[] chars = expression.toCharArray();
-        boolean b = hasBalanceParentheses(chars);
+        boolean b = hasBalanceParenthesesAscii(chars);
         System.out.println(b);
     }
 
@@ -56,6 +58,34 @@ public class Main {
             }
         }
 
+        return true;
+    }
+
+
+    public static boolean hasBalanceParenthesesAscii(char[] arr) {
+        StackADT<Character> stack = new StackImpl<Character>();
+
+        for (int i = 0; i < arr.length; i++) {
+            char c = arr[i];
+            Character pop;
+
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (c == ')' || c == ']' || c == '}') {
+                try {
+                    pop = stack.pop();
+                } catch (Exception e) {
+                    return false;
+                }
+                int popAscii = pop;
+                int currAscii = c;
+                if (currAscii == 41 && currAscii - 1 != popAscii) {
+                    return false;
+                } else if ((currAscii == 93 || currAscii == 125) && currAscii - 2 != popAscii) {
+                    return false;
+                }
+            }
+        }
         return true;
     }
 }

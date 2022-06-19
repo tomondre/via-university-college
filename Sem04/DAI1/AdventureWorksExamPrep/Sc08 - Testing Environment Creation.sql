88734346fb0047abfd4f9a837bdb3d709be9c54e
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-06-19 15:48:59 +0200

    Adding guide on how to execute the sql script

--- Sem04/DAI1/AdventureWorksExamPrep/Sc08 - Testing Environment Creation.sql
+++ Sem04/DAI1/AdventureWorksExamPrep/Sc08 - Testing Environment Creation.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6705128ce140a44cbcb355cf795f76189192bcfc7b248097bf13d7414cace2be
-size 572684512
+oid sha256:cff88f89ae4e7da6460be211bf25c265f74db4cbdc3f96141b2514aefe50808c
+size 572685520
